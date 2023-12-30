db3313ca21afa814caecae3a1d65077534667000
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-12-29 21:00:39 -0500

    Updates to unit tests

--- tests/main_testing.py
+++ tests/main_testing.py
@@ -105,3 +105,47 @@ if result.status_code == 200:
 else:
     logger.error(f"X Test 4 FAILED! Status Code: {str(result.status_code)}")
     quit()
+
+# Test account info update (Personalization)
+logger.info("Testing account info update (Personalization)...")
+
+print("Valid pronoun examples: 'he/him', 'she/her', 'they/them'")
+pronouns = input("Enter Pronouns: ")
+
+bio = input("Enter Bio: ")
+
+result = requests.post(url=f"{auth_url}/update_account_info/personalization", data={'username': username, "token": token, "pronouns": pronouns, "bio": bio})
+
+# Check status of operation
+if result.status_code == 200:
+    logger.info("✓ Test 5 PASSED!")
+
+else:
+    logger.error(f"X Test 5 FAILED! Status Code: {str(result.status_code)}")
+    quit()
+
+# Test get user bio
+logger.info("Testing get user bio...")
+
+result = requests.get(url=f"{auth_url}/get_user_bio/{username}")
+
+# Check status of operation
+if result.status_code == 200:
+    logger.info("✓ Test 6 PASSED!")
+
+else:
+    logger.error(f"X Test 6 FAILED! Status Code: {str(result.status_code)}")
+    quit()
+
+# Test get user pronouns
+logger.info("Testing get user pronouns...")
+
+result = requests.get(url=f"{auth_url}/get_user_pronouns/{username}")
+
+# Check status of operation
+if result.status_code == 200:
+    logger.info("✓ Test 6 PASSED!")
+
+else:
+    logger.error(f"X Test 7 FAILED! Status Code: {str(result.status_code)}")
+    quit()
